2f8fb40c635fde0a317e5ee92767b2b580ebef6f
tucuvaldez <105434510+tucuvaldez@users.noreply.github.com>
2022-08-11 21:49:42 +0200

    acomodo el codigo

--- asset/apps.js
+++ asset/apps.js
@@ -4,21 +4,12 @@ let total = document.getElementById("precioTotal");
 let cantidadCarrito = document.getElementById("cantidadCarrito");
 let cantEnCart = document.getElementById("cantEnCart");
 let botonEliminar = document.getElementById("botonEliminar")
-// const vinoTinto = listaVino.filter(vino => vino.tipoVino == "tinto");
-// console.log(vinoTinto);
-// const vinoBlanco = listaVino.filter(vino => vino.tipoVino == "blanco");
-// console.log(vinoBlanco);
+let listaVino
 let mostrar = document.getElementById("mostrarProductos");
 let filtros = document.getElementById("filtros")
-mostrar.addEventListener("click", (e) => {
-    contenedorProductos.style.display = 'flex';
-    filtros.style.display = 'block'
-})
-
 
 function setStorage(clave, valor) {
     localStorage.setItem(clave, JSON.stringify(valor))
-
 }
 function getStorage(clave) {
     return JSON.parse(localStorage.getItem(clave)) || []
@@ -27,69 +18,42 @@ function getStorage(clave) {
 const obtenerDatos = async () => {
     try {
         let response = await fetch('../asset/stock.json');
-        let data = await response.json();
-        let productos = data
-        productos.forEach(item => {
-            console.log(item.stock)
-            contenedorProductos.innerHTML += "";
-            let div = document.createElement("div");
-            div.className = "col-12 col-md-4 mt-3"
-            div.innerHTML = `<div class="card " style="width: 20rem">
-            <img class="card-img-top" src="${item.img}">
-            <div class="card-body">
-            <h4 class="card-title">${item.marca} </h4>
-            <h5 class="card-title">${item.varietal} </h5>
-            <p class="card-text">Enjoy the taste of the finest wine</p>
-            <p>$ ${item.precio}</p>
-            <button id="${item.id}" class="btnProd">Add to Cart</button>
-            </div>
-            </div>`
-            contenedorProductos.appendChild(div);
-
-            let btnAgregar = document.getElementById(`${item.id}`);
-            btnAgregar.addEventListener('click', () => {
-                cart(item)
-
-            })
-        })
+        listaVino = await response.json();
+        mostrarProducto(listaVino);
     } catch (error) {
         console.log(error)
     }
 }
 
-obtenerDatos()
-// function mostrarProducto(listaVino) {
-//     contenedorProductos.innerHTML = "";
-// listaVino.forEach(item => {
+function mostrarProducto(vinoAMostrar) {
+    contenedorProductos.innerHTML = "";
+    vinoAMostrar.forEach(item => {
+        let div = document.createElement("div");
+        div.className = "col-12 col-md-4 mt-3"
+        div.innerHTML = `<div class="card " style="width: 20rem">
+        <img class="card-img-top" src="${item.img}">
+        <div class="card-body">
+        <h4 class="card-title">${item.marca} </h4>
+        <h5 class="card-title">${item.varietal} </h5>
+        <p class="card-text">Enjoy the taste of the finest wine</p>
+        <p>$ ${item.precio}</p>
+        <button id="${item.id}" class="btnProd">Add to Cart</button>
+        </div>
+                        </div>`
+        contenedorProductos.appendChild(div);
 
-//     let div = document.createElement("div");
-//     div.className = "col-12 col-md-4 mt-3"
-//     div.innerHTML = `<div class="card " style="width: 20rem">
-//     <img class="card-img-top" src="${item.img}">
-//     <div class="card-body">
-//     <h4 class="card-title">${item.marca} </h4>
-//     <h5 class="card-title">${item.varietal} </h5>
-//     <p class="card-text">Enjoy the taste of the finest wine</p>
-//     <p>$ ${item.precio}</p>
-//     <button id="${item.id}" class="btnProd">Add to Cart</button>
-//     </div>
-//     </div>`
-//     contenedorProductos.appendChild(div);
+        let btnAgregar = document.getElementById(`${item.id}`);
+        btnAgregar.addEventListener('click', () => {
+            cart(item)
+        })
+    })
+}
 
-//     let btnAgregar = document.getElementById(`${item.id}`);
-//     btnAgregar.addEventListener('click', () => {
-//         cart(item)
-//     })
-// })
-// }
-
-
-
-// mostrarProducto(vinoTinto)
-// mostrarProducto(vinoBlanco)
-// mostrarProducto(listaVino);
-mostrarCarrito();
-contadorCarrito();
+function filtrar(e) {
+    let encontrado = listaVino.filter(vino => (vino.varietal).toLowerCase().includes(e.target.value) || (vino.marca).toLowerCase().includes(e.target.value))
+    mostrarProducto(encontrado);
+}
+window.addEventListener('input', filtrar);
 
 function cart(vino) {
     let carrito = getStorage('carrito');
@@ -101,7 +65,6 @@ function cart(vino) {
 
         })
     } else {
-
         vino.stock--
         if (carrito.some(item => item.id == vino.id)) {
 
@@ -109,21 +72,19 @@ function cart(vino) {
             carrito[index].cantidad++;
             carrito[index].stock--
         } else {
-
             carrito.push({ ...vino, cantidad: 1 });
         }
-
         setStorage('carrito', carrito);
         mostrarCarrito()
         contadorCarrito();
     }
 
 }
-cart()
+
 
 function mostrarCarrito() {
     let carrito = getStorage('carrito');
-    if (cantEnCart.innerText === '0') {
+    if (carrito.length === 0) {
         contenedorCarrito.innerText = 'Shopping cart empty'
     } else {
         contenedorCarrito.innerHTML = "";
@@ -131,12 +92,12 @@ function mostrarCarrito() {
             let div = document.createElement("div");
             div.className = "prodEnCarrito row";
             div.innerHTML = `
-                            <p class="col-3 align-items-end">${vino.marca}</p>
-                            <p class="col-3 align-items-end">${vino.varietal}</p>
-                            <p class="col-2 align-items-end">Precio: $${vino.precio} </p>  
+            <p class="col-3 align-items-end">${vino.marca}</p>
+            <p class="col-3 align-items-end">${vino.varietal}</p>
+            <p class="col-2 align-items-end">Precio: $${vino.precio} </p>  
                             <p id="cant${vino.id}" class="col-3 align-items-end">Quantity: ${vino.cantidad}</p>                    
                             <button id='button${vino.id}' class="col-1 btn-danger remove-btn"><i class="fa fa-trash-o fa-lg"></i></button>
-                        `
+                            `
             contenedorCarrito.appendChild(div);
 
             let btnEliminar = document.getElementById(`button${vino.id}`);
@@ -166,7 +127,6 @@ function eliminar(vino, btnEliminar) {
         carrito.splice(index, 1);
         setStorage('carrito', carrito);
         btnEliminar.parentElement.remove();
-        console.log(carrito);
     }
 
     vino.stock++
@@ -190,43 +150,6 @@ function checkDelete(vino, btnEliminar) {
     })
 }
 
-function limpiar() {
-    let carrito = getStorage('carrito');
-    botonEliminar.addEventListener('click', () => {
-
-        localStorage.clear(carrito);
-
-        contadorCarrito();
-        mostrarCarrito();
-    })
-}
-limpiar()
-
-function filtrar(e) {
-    //solo me filtra por uno solo, cuando agrego el or || no me funciona, no se que hice mal
-    let encontrado = listaVino.filter(vino => (vino.varietal).toLowerCase().includes(e.target.value) || (vino.marca).toLowerCase().includes(e.target.value))
-    mostrarProducto(encontrado);
-
-}
-window.addEventListener('input', filtrar);
-
-document.getElementById('checkTinto').addEventListener('change', (e) => {
-    if (e.target.checked == true) {
-        document.getElementById('checkBlanco').checked = false
-        mostrarProducto(vinoTinto)
-    } else {
-        mostrarProducto(listaVino)
-    }
-})
-document.getElementById('checkBlanco').addEventListener('change', (e) => {
-    if (e.target.checked == true) {
-        document.getElementById('checkTinto').checked = false
-        mostrarProducto(vinoBlanco)
-    } else {
-        mostrarProducto(listaVino)
-    }
-})
-
 function buy() {
     let btnComprar = document.getElementById('botonComprar')
     btnComprar.addEventListener('click', () => {
@@ -247,12 +170,15 @@ function buy() {
                 icon: 'success',
                 title: 'Thanks for your purchase'
             })
+            localStorage.clear();
+            contadorCarrito();
+            mostrarCarrito();
         } else {
             const Toast = Swal.mixin({
                 toast: true,
                 position: 'top-end',
                 showConfirmButton: false,
-                timer: 3000,
+                timer: 2000,
                 timerProgressBar: true,
                 didOpen: (toast) => {
                     toast.addEventListener('mouseenter', Swal.stopTimer)
@@ -264,10 +190,10 @@ function buy() {
                 icon: 'error',
                 title: 'Shopping cart it is empty'
             })
+
         }
     })
 }
-buy()
 
 function contadorCarrito() {
     let carrito = getStorage('carrito');
@@ -276,3 +202,43 @@ function contadorCarrito() {
     total.innerText = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
 }
 
+mostrar.addEventListener("click", (e) => {
+    contenedorProductos.style.display = 'flex';
+    filtros.style.display = 'block'
+})
+
+let carrito = getStorage('carrito');
+botonEliminar.addEventListener('click', () => {
+
+    localStorage.clear();
+
+    contadorCarrito();
+    mostrarCarrito();
+})
+
+document.getElementById('checkTinto').addEventListener('change', (e) => {
+
+    if (e.target.checked == true) {
+        const vinoTinto = listaVino.filter(vino => vino.tipoVino == "tinto");
+        document.getElementById('checkBlanco').checked = false
+        mostrarProducto(vinoTinto)
+    } else {
+        mostrarProducto(listaVino)
+    }
+})
+document.getElementById('checkBlanco').addEventListener('change', (e) => {
+    if (e.target.checked == true) {
+        const vinoBlanco = listaVino.filter(vino => vino.tipoVino == "blanco");
+        document.getElementById('checkTinto').checked = false
+        mostrarProducto(vinoBlanco)
+    } else {
+        if (!document.getElementById('checkTinto').checked) {
+            mostrarProducto(listaVino)
+        }
+    }
+})
+
+obtenerDatos()
+mostrarCarrito();
+contadorCarrito();
+buy()
